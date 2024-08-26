05e140aeade86f03698b7444ffc8ce320d847547
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2024-08-26 09:09:05 +0200

    final DatabaseConnectionSupplier

--- operations/src/main/java/no/sikt/nva/handle/utils/DatabaseConnectionSupplier.java
+++ operations/src/main/java/no/sikt/nva/handle/utils/DatabaseConnectionSupplier.java
@@ -11,7 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @JacocoGenerated
-public class DatabaseConnectionSupplier {
+public final class DatabaseConnectionSupplier {
     public static final String ENV_DATABASE_PASSWORD = "DATABASE_PASSWORD";
     public static final String ENV_DATABASE_USER = "DATABASE_USER";
     public static final String ENV_DATABASE_URI = "DATABASE_URI";
