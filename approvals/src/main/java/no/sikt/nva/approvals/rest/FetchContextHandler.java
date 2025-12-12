9a3deb398ff9165376f202bb86525039d3386d48
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2025-12-12 11:14:55 +0100

    feat(approvals): add JSON-LD context and RDF ontology endpoints #NP-50457
    Add @context field to ApprovalResponse pointing to /approval/context endpoint.
    Add /context and /ontology endpoints for serving JSON-LD context and RDF ontology.

---
+++ approvals/src/main/java/no/sikt/nva/approvals/rest/FetchContextHandler.java
@@ -0,0 +1,51 @@
+package no.sikt.nva.approvals.rest;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.common.net.MediaType;
+import nva.commons.apigateway.ApiGatewayHandler;
+import nva.commons.apigateway.RequestInfo;
+import nva.commons.apigateway.exceptions.ApiGatewayException;
+import nva.commons.core.Environment;
+import nva.commons.core.JacocoGenerated;
+import nva.commons.core.ioutils.IoUtils;
+
+import java.net.HttpURLConnection;
+import java.nio.file.Path;
+import java.util.List;
+
+import static com.google.common.net.MediaType.JSON_UTF_8;
+import static nva.commons.apigateway.MediaTypes.APPLICATION_JSON_LD;
+
+public class FetchContextHandler extends ApiGatewayHandler<Void, String> {
+
+    private static final String CONTEXT = IoUtils.stringFromResources(Path.of("approval-context.json"));
+
+    @JacocoGenerated
+    public FetchContextHandler() {
+        this(new Environment());
+    }
+
+    public FetchContextHandler(Environment environment) {
+        super(Void.class, environment);
+    }
+
+    @Override
+    protected String processInput(Void input, RequestInfo requestInfo, Context context) throws ApiGatewayException {
+        return CONTEXT;
+    }
+
+    @Override
+    protected Integer getSuccessStatusCode(Void input, String output) {
+        return HttpURLConnection.HTTP_OK;
+    }
+
+    @Override
+    protected List<MediaType> listSupportedMediaTypes() {
+        return List.of(APPLICATION_JSON_LD, JSON_UTF_8);
+    }
+
+    @Override
+    protected void validateRequest(Void unused, RequestInfo requestInfo, Context context) throws ApiGatewayException {
+        // No validation needed
+    }
+}
