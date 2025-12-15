c0ae8f6d59bee0d4fdaa35ff7aa19f224602a030
Torbjørn Kvåle <torbjorn.kvale@sikt.no>
2025-12-15 10:35:19 +0100

    fix(approvals): use Lambda for context and ontology endpoints #NP-50457

---
+++ approvals/src/test/java/no/sikt/nva/approvals/rest/FetchContextHandlerTest.java
@@ -0,0 +1,62 @@
+package no.sikt.nva.approvals.rest;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import no.unit.nva.commons.json.JsonUtils;
+import no.unit.nva.stubs.FakeContext;
+import no.unit.nva.testutils.HandlerRequestBuilder;
+import nva.commons.apigateway.GatewayResponse;
+import nva.commons.core.ioutils.IoUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.nio.file.Path;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.StringContains.containsString;
+
+class FetchContextHandlerTest {
+
+    private static final String EXPECTED_CONTEXT = IoUtils.stringFromResources(Path.of("approval-context.json"));
+    private static final Context CONTEXT = new FakeContext();
+    private FetchContextHandler handler;
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    void setUp() {
+        handler = new FetchContextHandler();
+        outputStream = new ByteArrayOutputStream();
+    }
+
+    @Test
+    void shouldReturnContextJson() throws IOException {
+        var inputStream = createRequest();
+
+        handler.handleRequest(inputStream, outputStream, CONTEXT);
+
+        var response = GatewayResponse.fromOutputStream(outputStream, String.class);
+        assertThat(response.getStatusCode(), is(HttpURLConnection.HTTP_OK));
+        assertThat(response.getBody(), containsString("@context"));
+        assertThat(response.getBody(), containsString("@vocab"));
+        assertThat(response.getBody(), containsString("https://nva.unit.no/approval#"));
+    }
+
+    @Test
+    void shouldReturnExpectedContextContent() throws IOException {
+        var inputStream = createRequest();
+
+        handler.handleRequest(inputStream, outputStream, CONTEXT);
+
+        var response = GatewayResponse.fromOutputStream(outputStream, String.class);
+        assertThat(response.getBody(), is(EXPECTED_CONTEXT));
+    }
+
+    private InputStream createRequest() throws JsonProcessingException {
+        return new HandlerRequestBuilder<Void>(JsonUtils.dtoObjectMapper).build();
+    }
+}
